f1bf853cfc0f8e845d46ef411fafea2f5b49a984
Matheus Marques <matheuspadilha.dev@gmail.com>
2023-04-27 21:04:42 -0300

    first commit

---
+++ src/main/java/whalewarehouse/WhaleWarehouseApplication.java
@@ -0,0 +1,13 @@
+package whalewarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhaleWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhaleWarehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/whalewarehouse/WhaleWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package whalewarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhaleWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
