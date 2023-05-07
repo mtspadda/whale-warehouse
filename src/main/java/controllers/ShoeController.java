98a5e39f8b09522626cb75a8160f2ac3c31b1908
Matheus Marques <matheuspadilha.dev@gmail.com>
2023-05-07 14:59:14 -0300

    docker configuration, postgres db configuration

--- src/main/java/controllers/ShoeController.java
+++ src/main/java/controllers/ShoeController.java
@@ -1,20 +1,23 @@
 package controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import services.ShoeService;
 import whalewarehouse.Shoe;
 
 import java.util.List;
-
+@RestController
 @RequestMapping(path = "api/v1/shoe")
 public class ShoeController {
 
+    private final ShoeService shoeService;
+    @Autowired
     public ShoeController(ShoeService shoeService) {
         this.shoeService = shoeService;
     }
 
-    private final ShoeService shoeService;
 
     @GetMapping
     public List<Shoe> getAllShoes(){
--- src/main/java/services/ShoeService.java
+++ src/main/java/services/ShoeService.java
@@ -1,9 +1,10 @@
 package services;
 
+import org.springframework.stereotype.Component;
 import whalewarehouse.Shoe;
 
 import java.util.List;
-
+@Component
 public class ShoeService {
     public List<Shoe> getAllShoes(){
         return List.of(new Shoe(1L, "7899999876", "Sneaker"));
