88ffc3be7b28c6ccb5685270a9fd33322c3de765
Matheus Marques <matheuspadilha.dev@gmail.com>
2023-10-18 00:11:04 -0300

    Change for a repository for each model

--- src/main/java/repository/BasicRepository.java
+++
@@ -1,6 +0,0 @@
-package repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BasicRepository <T extends Class> extends JpaRepository<T, Long> {
-}
--- src/main/java/service/BrandService.java
+++ src/main/java/service/BrandService.java
@@ -2,15 +2,15 @@ package service;
 
 import model.Brand;
 import org.springframework.stereotype.Service;
-import repository.BasicRepository;
+import repository.BrandRepository;
 
 @Service
 public class BrandService extends GenericService<Brand, Long> {
 
-    private BasicRepository repository;
+    private BrandRepository repository;
 
 
-    public BrandService(BasicRepository repository){
+    public BrandService(BrandRepository repository){
         super(repository);
         this.repository = repository;
     }
